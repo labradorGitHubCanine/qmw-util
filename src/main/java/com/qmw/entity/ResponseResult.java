59fffd469bd6e71651aebb2b935cf02b621a99d5
labradoGithubCanine <123349527@qq.com>
2020-08-15 02:07:49 +0800

    ResponseResult增加泛型，并可自定义各状态的返回码

--- src/main/java/com/qmw/entity/ResponseResult.java
+++ src/main/java/com/qmw/entity/ResponseResult.java
@@ -6,49 +6,72 @@ package com.qmw.entity;
  * @author qmw
  * @since 1.00
  */
-public class ResponseResult {
+public class ResponseResult<T> {
+
+    private static boolean initialized; // 是否已经初始化
+
+    // 成功，失败，需要重新登录，需要更新，接口/链接失效
+    private static int OK, ERROR, RELOGIN, UPGRADE, INVALID;
 
     private int code;
-    private String msg;
-    private String token;
-    private Object data;
+    private String msg, token;
+    private T data;
 
     private ResponseResult() {
+        if (!initialized)
+            throw new RuntimeException("请先调用" + this.getClass().getName() + ".init方法进行初始化（只需调用一次）");
     }
 
-    public static ResponseResult ok(Object data, String msg) {
-        return new ResponseResult().setCode(1).setMsg(msg).setData(data);
+    public static void init(int ok, int error, int relogin, int upgrade, int invalid) {
+        initialized = true;
+        OK = ok;
+        ERROR = error;
+        RELOGIN = relogin;
+        UPGRADE = upgrade;
+        INVALID = invalid;
     }
 
-    public static ResponseResult ok(Object data) {
+    public static <T> ResponseResult<T> ok(T data, String msg) {
+        return new ResponseResult<T>().setCode(OK).setMsg(msg).setData(data);
+    }
+
+    public static <T> ResponseResult<T> ok(T data) {
         return ok(data, "操作成功");
     }
 
-    public static ResponseResult ok(String msg) {
+    public static <T> ResponseResult<T> ok(String msg) {
         return ok(null, msg);
     }
 
-    public static ResponseResult ok() {
+    public static <T> ResponseResult<T> ok() {
         return ok(null, "操作成功");
     }
 
-    public static ResponseResult error(String msg) {
-        return new ResponseResult().setCode(0).setMsg(msg);
+    public static <T> ResponseResult<T> error(String msg) {
+        return new ResponseResult<T>().setCode(ERROR).setMsg(msg);
     }
 
-    public static ResponseResult error() {
+    public static <T> ResponseResult<T> error() {
         return error("操作失败");
     }
 
-    public static ResponseResult relogin(String msg) {
-        return new ResponseResult().setCode(1000).setMsg(msg);
+    public static <T> ResponseResult<T> relogin(String msg) {
+        return new ResponseResult<T>().setCode(RELOGIN).setMsg(msg);
+    }
+
+    public static <T> ResponseResult<T> upgrade(String msg) {
+        return new ResponseResult<T>().setCode(UPGRADE).setMsg(msg);
+    }
+
+    public static <T> ResponseResult<T> invalid(String msg) {
+        return new ResponseResult<T>().setCode(INVALID).setMsg(msg);
     }
 
     public int getCode() {
         return code;
     }
 
-    public ResponseResult setCode(int code) {
+    public ResponseResult<T> setCode(int code) {
         this.code = code;
         return this;
     }
@@ -57,7 +80,7 @@ public class ResponseResult {
         return msg;
     }
 
-    public ResponseResult setMsg(String msg) {
+    public ResponseResult<T> setMsg(String msg) {
         this.msg = msg;
         return this;
     }
@@ -66,16 +89,16 @@ public class ResponseResult {
         return token;
     }
 
-    public ResponseResult setToken(String token) {
+    public ResponseResult<T> setToken(String token) {
         this.token = token;
         return this;
     }
 
-    public Object getData() {
+    public T getData() {
         return data;
     }
 
-    public ResponseResult setData(Object data) {
+    public ResponseResult<T> setData(T data) {
         this.data = data;
         return this;
     }
