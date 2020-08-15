3f5db99097ba6629facffc6c42a340395586f017
labradoGithubCanine <123349527@qq.com>
2020-08-16 00:13:54 +0800

    ResponseResult优化

--- src/main/java/com/qmw/entity/ResponseResult.java
+++ src/main/java/com/qmw/entity/ResponseResult.java
@@ -8,28 +8,22 @@ package com.qmw.entity;
  */
 public class ResponseResult<T> {
 
-    private static boolean initialized; // 是否已经初始化
-
-    // 成功，失败，需要重新登录，需要更新，接口/链接失效
-    private static int OK, ERROR, RELOGIN, UPGRADE, INVALID;
+    private static int
+            OK = 1, // 成功
+            ERROR = 0, // 失败
+            RELOGIN = 1000, // 需要重新登录
+            UPGRADE = 1001, // 需要更新
+            INVALID = 1002, // 接口/链接失效
+            UNAUTHORIZED = 1003; // 没有权限
 
     private int code;
     private String msg, token;
     private T data;
 
     private ResponseResult() {
-        if (!initialized)
-            throw new RuntimeException("请先调用" + this.getClass().getName() + ".init方法进行初始化（只需调用一次）");
     }
 
-    public static void init(int ok, int error, int relogin, int upgrade, int invalid) {
-        initialized = true;
-        OK = ok;
-        ERROR = error;
-        RELOGIN = relogin;
-        UPGRADE = upgrade;
-        INVALID = invalid;
-    }
+    // ---------- 静态构造
 
     public static <T> ResponseResult<T> ok(T data, String msg) {
         return new ResponseResult<T>().setCode(OK).setMsg(msg).setData(data);
@@ -67,6 +61,12 @@ public class ResponseResult<T> {
         return new ResponseResult<T>().setCode(INVALID).setMsg(msg);
     }
 
+    public static <T> ResponseResult<T> unauthorized(String msg) {
+        return new ResponseResult<T>().setCode(UNAUTHORIZED).setMsg(msg);
+    }
+
+    // ---------- getters and setters
+
     public int getCode() {
         return code;
     }
@@ -101,6 +101,32 @@ public class ResponseResult<T> {
     public ResponseResult<T> setData(T data) {
         this.data = data;
         return this;
+    }
+
+    // ---------- 自定义状态码
+
+    public static void setOK(int OK) {
+        ResponseResult.OK = OK;
+    }
+
+    public static void setERROR(int ERROR) {
+        ResponseResult.ERROR = ERROR;
+    }
+
+    public static void setRELOGIN(int RELOGIN) {
+        ResponseResult.RELOGIN = RELOGIN;
+    }
+
+    public static void setUPGRADE(int UPGRADE) {
+        ResponseResult.UPGRADE = UPGRADE;
+    }
+
+    public static void setINVALID(int INVALID) {
+        ResponseResult.INVALID = INVALID;
+    }
+
+    public static void setUNAUTHORIZED(int UNAUTHORIZED) {
+        ResponseResult.UNAUTHORIZED = UNAUTHORIZED;
     }
 
 }
