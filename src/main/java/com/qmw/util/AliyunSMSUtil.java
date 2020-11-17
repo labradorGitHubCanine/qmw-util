be2b320079efeed4459c79027c89957824add785
labradorGitHubCanine <123349527@qq.com>
2020-11-17 09:53:33 +0800

    随便改改

--- src/main/java/com/qmw/util/AliyunSMSUtil.java
+++ src/main/java/com/qmw/util/AliyunSMSUtil.java
@@ -8,11 +8,12 @@ import com.aliyuncs.IAcsClient;
 import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
-import com.qmw.entity.AliyunConfig;
 import com.qmw.exception.CustomException;
+import lombok.Data;
+import lombok.experimental.Accessors;
 
-import java.util.List;
-import java.util.Map;
+import java.util.Collections;
+import java.util.HashMap;
 
 /**
  * 阿里云短信工具类
@@ -22,20 +23,22 @@ import java.util.Map;
  */
 public class AliyunSMSUtil {
 
-    private static AliyunConfig config;
+    private static String ACCESS_KEY_ID;
+    private static String ACCESS_KEY_SECRET;
+    private static String SIGN_NAME; // 短信签名：例如【顺丰】
 
-    public static void init(AliyunConfig config) {
-        if (StringUtil.isEmpty(config.getAccessKeyId()))
-            throw new CustomException("请设置accessKeyId");
-        if (StringUtil.isEmpty(config.getAccessKeySecret()))
-            throw new CustomException("请设置accessKeySecret");
-        if (StringUtil.isEmpty(config.getSignName()))
-            throw new CustomException("请设置signName");
-        AliyunSMSUtil.config = config;
+    // 初始化参数，可在项目启动时调用一次
+    public static void init(String accessKeyId, String accessKeySecret, String signName) {
+        ACCESS_KEY_ID = accessKeyId;
+        ACCESS_KEY_SECRET = accessKeySecret;
+        SIGN_NAME = signName;
     }
 
-    public static Result send(List<String> phoneNumbers, String templateCode, Map<String, Object> params) {
-        DefaultProfile profile = DefaultProfile.getProfile("default", config.getAccessKeyId(), config.getAccessKeySecret());
+    public static Result send(Iterable<String> phoneNumbers, String templateCode, Params params) {
+        if (ACCESS_KEY_ID == null || ACCESS_KEY_SECRET == null || SIGN_NAME == null)
+            throw new CustomException("请先调用init方法进行初始化");
+
+        DefaultProfile profile = DefaultProfile.getProfile("default", ACCESS_KEY_ID, ACCESS_KEY_SECRET);
         IAcsClient client = new DefaultAcsClient(profile);
 
         CommonRequest request = new CommonRequest();
@@ -44,62 +47,57 @@ public class AliyunSMSUtil {
         request.setSysVersion("2017-05-25");
         request.setSysAction("SendSms");
         request.putQueryParameter("PhoneNumbers", String.join(",", phoneNumbers));
-        request.putQueryParameter("SignName", config.getSignName());
+        request.putQueryParameter("SignName", SIGN_NAME);
         request.putQueryParameter("TemplateCode", templateCode);
-        request.putQueryParameter("TemplateParam", JSON.toJSONString(params));
+        request.putQueryParameter("TemplateParam", JSON.toJSONString(params.getMap()));
         try {
             CommonResponse response = client.getCommonResponse(request);
             return JSON.parseObject(response.getData(), Result.class);
         } catch (ClientException e) {
             e.printStackTrace();
-            throw new CustomException(e.getMessage());
+            return new Result().setMessage(e.getMessage());
         }
+    }
+
+    public static Result send(Iterable<String> phoneNumbers, String templateCode) {
+        return send(phoneNumbers, templateCode, new Params());
+    }
+
+    public static Result send(String phoneNumber, String templateCode, Params params) {
+        return send(Collections.singletonList(phoneNumber), templateCode, params);
+    }
+
+    public static Result send(String phoneNumber, String templateCode) {
+        return send(Collections.singletonList(phoneNumber), templateCode, new Params());
     }
 
     // 请求参数类
     public static class Params {
 
+        private HashMap<String, Object> map;
+
+        public Params put(String key, Object value) {
+            if (map == null)
+                map = new HashMap<>();
+            map.put(key, value);
+            return this;
+        }
+
+        public HashMap<String, Object> getMap() {
+            return map == null ? new HashMap<>() : map;
+        }
+
     }
 
     // 阿里云短信结果类
+    @Data
+    @Accessors(chain = true)
     public static class Result {
 
         private String Message;
         private String RequestId;
         private String BizId;
         private String Code;
-
-        public String getMessage() {
-            return Message;
-        }
-
-        public void setMessage(String message) {
-            Message = message;
-        }
-
-        public String getRequestId() {
-            return RequestId;
-        }
-
-        public void setRequestId(String requestId) {
-            RequestId = requestId;
-        }
-
-        public String getBizId() {
-            return BizId;
-        }
-
-        public void setBizId(String bizId) {
-            BizId = bizId;
-        }
-
-        public String getCode() {
-            return Code;
-        }
-
-        public void setCode(String code) {
-            Code = code;
-        }
 
     }
 
--- src/main/java/com/qmw/util/ExcelUtil.java
+++ src/main/java/com/qmw/util/ExcelUtil.java
@@ -1,9 +1,16 @@
 package com.qmw.util;
 
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
+import com.alibaba.excel.write.metadata.WriteSheet;
+import com.alibaba.excel.write.metadata.style.WriteCellStyle;
+import com.alibaba.excel.write.metadata.style.WriteFont;
+import com.alibaba.excel.write.style.HorizontalCellStyleStrategy;
+import com.alibaba.excel.write.style.column.LongestMatchColumnWidthStyleStrategy;
 import com.qmw.exception.CustomException;
+import org.apache.poi.ss.usermodel.IndexedColors;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -11,6 +18,8 @@ import java.io.InputStream;
 import java.math.BigDecimal;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.*;
 
 /**
@@ -23,82 +32,133 @@ import java.util.*;
 public class ExcelUtil {
 
     /**
-     * 将生成的excel返回给web前端
+     * 将一个map写入到excel中并返回给前端
      *
-     * @param list     list
+     * @param map      map的key是sheetName，value是内容
      * @param response response
-     * @param fileName 文件名，可以为空
+     * @param fileName 文件名
      */
-    public static void download(List<? extends Map<String, ?>> list, HttpServletResponse response, String fileName) {
+    public static void download(Map<String, List<? extends Map<String, ?>>> map, HttpServletResponse response, String fileName) {
         try {
-            List<List<String>> head = new ArrayList<>(); // 表头
-            List<List<Object>> data = new ArrayList<>(); // 内容
-            if (list != null && !list.isEmpty()) {
-                list.get(0).keySet().forEach(i -> head.add(Collections.singletonList(i)));
-                list.forEach(i -> {
-                    List<Object> row = new ArrayList<>();
-                    i.values().forEach(j -> {
-                        if (j == null)
-                            row.add("");
-                        else if (j instanceof BigDecimal)
-                            row.add(((BigDecimal) j).toPlainString());
-                        else
-                            row.add(j.toString());
-                    });
-                    data.add(row);
-                });
-            }
+            WriteCellStyle headStyle = new WriteCellStyle(); // 重写头部样式
+            headStyle.setFillForegroundColor(IndexedColors.WHITE.getIndex()); // 白色背景
+            headStyle.setWrapped(false); // 不自动换行
+            WriteFont headFont = new WriteFont();
+            headFont.setFontHeightInPoints((short) 12); // 12号字
+            headStyle.setWriteFont(headFont);
+
             fileName = URLEncoder.encode(StringUtil.ifEmptyThen(fileName, UUID.randomUUID().toString()) + ".xlsx", StandardCharsets.UTF_8.name());
             response.setContentType("application/x-download");
             response.setHeader("Access-Control-Expose-Headers", "Content-Disposition"); // 允许设置Content-Disposition
             response.addHeader("Content-Disposition", "attachment;filename=" + fileName);
-            EasyExcel.write(response.getOutputStream()).sheet("sheet1").head(head).doWrite(data);
+//            ExcelWriter writer = EasyExcel.write("C:\\Users\\12334\\Desktop\\" + fileName)
+            ExcelWriter writer = EasyExcel.write(response.getOutputStream())
+                    .registerWriteHandler(new HorizontalCellStyleStrategy(headStyle, new WriteCellStyle())) // 注册自定义样式
+//                    .registerWriteHandler(new LongestMatchColumnWidthStyleStrategy()) // 自动列宽
+                    .build();
+
+            if (map != null) {
+                int a = 0;
+                for (Map.Entry<String, List<? extends Map<String, ?>>> e : map.entrySet()) {
+                    String sheetName = e.getKey();
+                    List<? extends Map<String, ?>> list = e.getValue();
+                    List<List<String>> head = new ArrayList<>(); // 表头
+                    List<List<Object>> data = new ArrayList<>(); // 内容
+                    if (list != null && !list.isEmpty()) {
+                        // 取所有表头的并集
+                        LinkedHashSet<String> set = new LinkedHashSet<>();
+                        list.forEach(i -> set.addAll(i.keySet()));
+                        set.forEach(i -> head.add(Collections.singletonList(i))); // 单级表头用单元素数组
+                        set.clear(); // 释放内存
+                        // 写入内容
+                        list.forEach(i -> {
+                            List<Object> row = new ArrayList<>();
+                            head.forEach(j -> {
+                                Object value = i.get(j.get(0));
+                                if (value instanceof Number) // 防止变成科学计数
+                                    value = new BigDecimal(value.toString()).toPlainString();
+                                else if (value instanceof Date) // 不转换会报错
+                                    value = value.toString();
+                                else if (value instanceof Timestamp) // 不转换会报错
+                                    value = value.toString();
+                                row.add(value);
+                            });
+                            data.add(row);
+                        });
+                    }
+                    // 写入sheet中
+                    WriteSheet writeSheet = EasyExcel.writerSheet(a, sheetName).head(head).build();
+                    writer.write(data, writeSheet);
+                    a++;
+                }
+            }
+            writer.finish();
         } catch (IOException e) {
             e.printStackTrace();
             throw new CustomException("文件写入失败！");
         }
     }
 
-    /**
-     * 将生成的excel返回给web前端
-     *
-     * @param list     list
-     * @param response response
-     */
+    public static void download(Map<String, List<? extends Map<String, ?>>> map, HttpServletResponse response) {
+        download(map, response, null);
+    }
+
+    public static void download(List<? extends Map<String, ?>> list, HttpServletResponse response, String fileName) {
+        download(new HashMap<String, List<? extends Map<String, ?>>>() {{
+            put("sheet1", list);
+        }}, response, fileName);
+    }
+
     public static void download(List<? extends Map<String, ?>> list, HttpServletResponse response) {
-        download(list, response, "");
+        download(new HashMap<String, List<? extends Map<String, ?>>>() {{
+            put("sheet1", list);
+        }}, response, null);
     }
 
     /**
-     * 将excel解析为List
+     * 读取第一个sheet，并放入一个list中返回
      *
      * @param stream        文件流
-     * @param headRowNumber 表头开始的行数
+     * @param headRowNumber 表头从第几行开始
      * @return List
      */
-    public static List<Map<String, String>> readAsList(InputStream stream, int headRowNumber) {
+    public static List<LinkedHashMap<String, String>> readAsList(InputStream stream, int headRowNumber) {
         Objects.requireNonNull(stream);
         DataListener listener = new DataListener();
         EasyExcel.read(stream, listener).sheet().headRowNumber(headRowNumber).doRead();
-        return listener.getList();
+        return listener.result().values().iterator().next();
     }
 
-    public static List<Map<String, String>> readAsList(InputStream stream) {
+    public static List<LinkedHashMap<String, String>> readAsList(InputStream stream) {
         return readAsList(stream, 1);
+    }
+
+    /**
+     * 读取所有的sheet，并放入一个map中返回，map的key为sheetName
+     *
+     * @param stream 文件流
+     * @return Map
+     */
+    public static LinkedHashMap<String, List<LinkedHashMap<String, String>>> readAsMap(InputStream stream) {
+        Objects.requireNonNull(stream);
+        DataListener listener = new DataListener();
+        EasyExcel.read(stream, listener).doReadAll();
+        return listener.result();
     }
 
     private static class DataListener extends AnalysisEventListener<Map<Integer, String>> {
 
-        private final List<Map<String, String>> list = new ArrayList<>();
+        private final LinkedHashMap<String, List<LinkedHashMap<String, String>>> map = new LinkedHashMap<>(); // sheetName - list
+        private final List<LinkedHashMap<String, String>> list = new ArrayList<>(); // list
         private Map<Integer, String> headMap;
 
-        public List<Map<String, String>> getList() {
-            return this.list;
+        public LinkedHashMap<String, List<LinkedHashMap<String, String>>> result() {
+            return this.map;
         }
 
         @Override
         public void invoke(Map<Integer, String> data, AnalysisContext context) {
-            Map<String, String> map = new HashMap<>();
+            LinkedHashMap<String, String> map = new LinkedHashMap<>();
             data.forEach((key, value) -> {
                 if (value != null)
                     map.put(headMap.get(key), value.trim());
@@ -112,38 +172,12 @@ public class ExcelUtil {
             this.headMap = headMap;
         }
 
+        // 每读完一个sheet都会执行此方法
         @Override
         public void doAfterAllAnalysed(AnalysisContext context) {
+            map.put(context.readSheetHolder().getSheetName(), new ArrayList<>(list));
+            list.clear();
         }
-
     }
-
-//    public static void main(String[] args) {
-//        Workbook workbook = new XSSFWorkbook();
-//        Sheet sheet = workbook.createSheet();
-//        Cell cell = sheet.createRow(0).createCell(0);
-//        cell.setCellValue(123);
-//
-//        CellStyle style = workbook.createCellStyle();
-//        style.setFillForegroundColor(IndexedColors.SKY_BLUE.getIndex());
-//        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-//
-//        cell.setCellStyle(style);
-//
-//        FileOutputStream out = null;
-//        try {
-//            out = new FileOutputStream("C:\\Users\\12334\\Desktop\\1.xlsx");
-//            workbook.write(out);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                if (out != null)
-//                    out.close();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
 
 }
