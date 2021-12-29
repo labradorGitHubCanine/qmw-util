00586a45b616af8595209338b5010f3e077a3afd
labradorGitHubCanine <123349527@qq.com>
2021-12-29 11:04:15 +0800

    增加注解AuthentificationIgnore

---
+++ src/main/java/com/qmw/annotation/AuthentificationIgnore.java
@@ -0,0 +1,9 @@
+package com.qmw.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface AuthentificationIgnore {
+}
