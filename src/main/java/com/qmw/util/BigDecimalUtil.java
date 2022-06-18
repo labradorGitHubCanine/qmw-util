ffe285897c6ef7440020e9521315eda2fb159064
qmw <123349527@qq.com>
2022-06-18 10:39:14 +0800

    0

--- src/main/java/com/qmw/util/BigDecimalUtil.java
+++ src/main/java/com/qmw/util/BigDecimalUtil.java
@@ -7,28 +7,26 @@ import java.util.Arrays;
 public class BigDecimalUtil {
 
     public static BigDecimal add(Object... objects) {
-        BigDecimal sum = BigDecimal.ZERO;
-        for (Object o : objects)
-            sum = sum.add(new BigDecimal(String.valueOf(o)));
-        return sum;
+        return Arrays.stream(objects).map(o -> new BigDecimal(o.toString())).reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
-    public static BigDecimal divide(int scale, Object... objects) {
-        BigDecimal divisor = new BigDecimal(objects[0].toString());
-
-        BigDecimal dividend = multiply(Arrays.stream(objects).s)
-        for (int i = 1; i < objects.length; i++)
-            dividend = dividend.multiply(new BigDecimal(String.valueOf(o)));
-
-
-        return bigDecimal.setScale(scale, RoundingMode.HALF_UP);
+    public static BigDecimal subtract(Object o1, Object... o2) {
+        BigDecimal minuend = new BigDecimal(o1.toString());
+        BigDecimal subtraction = add(o2);
+        return minuend.subtract(subtraction);
     }
 
     public static BigDecimal multiply(Object... objects) {
-        BigDecimal sum = BigDecimal.ZERO;
+        BigDecimal sum = BigDecimal.ONE;
         for (Object o : objects)
             sum = sum.multiply(new BigDecimal(String.valueOf(o)));
         return sum;
+    }
+
+    public static BigDecimal divide(int scale, Object o1, Object... o2) {
+        BigDecimal divisor = new BigDecimal(o1.toString());
+        BigDecimal dividend = multiply(o2);
+        return divisor.divide(dividend, scale, RoundingMode.HALF_UP);
     }
 
 }
